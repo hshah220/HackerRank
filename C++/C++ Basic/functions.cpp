927be1558ee59c418801526ab8b6d5fa2bdce021
Hemil Shah <47408739+hshah220@users.noreply.github.com>
2024-05-21 15:14:29 -0500

    Create functions.cpp

---
+++ C++/C++ Basic/functions.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+
+int max_of_four(int a, int b, int c, int d){
+    int greatest;
+    greatest = a;
+    if(b > greatest){
+        greatest = b;
+    }
+    if(c > greatest){
+        greatest = c;
+    }
+    if(d > greatest){
+        greatest = d;
+    }
+    return greatest;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
